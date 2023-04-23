58a91f42e7441b6a2245a349aee084af0ea0d23c
Vin Norman <vin@edevolearning.com>
2023-04-23 09:43:33 +0100

    Tidy theme file

--- app/src/main/java/com/gonativecoders/whosin/core/theme/Theme.kt
+++ app/src/main/java/com/gonativecoders/whosin/core/theme/Theme.kt
@@ -26,8 +26,6 @@ private val LightColorScheme = lightColorScheme(
     background = Color.White
 
     /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
     onPrimary = Color.White,
     onSecondary = Color.White,
     onTertiary = Color.White,
@@ -39,15 +37,9 @@ private val LightColorScheme = lightColorScheme(
 @Composable
 fun WhosInTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
-    // Dynamic color is available on Android 12+
-    dynamicColor: Boolean = true,
     content: @Composable () -> Unit
 ) {
     val colorScheme = when {
-//        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
-//            val context = LocalContext.current
-//            if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
-//        }
 //        darkTheme -> DarkColorScheme
         else -> LightColorScheme
     }
--- app/src/main/java/com/gonativecoders/whosin/ui/MainActivity.kt
+++ app/src/main/java/com/gonativecoders/whosin/ui/MainActivity.kt
@@ -1,12 +1,11 @@
-@file:OptIn(ExperimentalMaterial3Api::class)
-
 package com.gonativecoders.whosin.ui
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.*
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.navigation.compose.NavHost
