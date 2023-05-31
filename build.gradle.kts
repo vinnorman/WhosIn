885134e1bda457a4aea35b603e3cf4925af28b30
Vin Norman <vin@edevolearning.com>
2023-05-31 10:52:36 +0100

    Fix for crashlytics

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,4 +6,5 @@ plugins {
     id("com.android.library") version "8.0.1" apply false
     id("org.jetbrains.kotlin.android") version "1.8.21" apply false
     id("com.google.gms.google-services") version "4.3.14" apply false
+    id("com.google.firebase.crashlytics") version "2.9.5" apply false
 }
