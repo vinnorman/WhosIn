4ce0655f50d3b6b62dc70771bff8fce678cc07db
Vin Norman <vin@edevolearning.com>
2023-06-07 12:45:22 +0100

    Fix bug in pipeline versioning

--- buildSrc/src/main/kotlin/Versioning.kt
+++ buildSrc/src/main/kotlin/Versioning.kt
@@ -31,7 +31,7 @@ fun Project.generateVersionCode(): Int {
 private fun Project.gitDescribe(): Pair<String, String?> {
     val byte = ByteArrayOutputStream()
     project.exec {
-        commandLine = listOf("git", "describe", " --tags")
+        commandLine = "git describe --tags".split("")
         standardOutput = byte
     }
     val result = String(byte.toByteArray()).trim().split("-")
